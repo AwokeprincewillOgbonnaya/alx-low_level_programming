fe09074dbbcc59abbf47661e4f7725cc80bf624a
AwokeprincewillOgbonnaya <princewillawoke@gmail.com>
2023-07-13 17:05:31 -0700

    Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point 0 (success)
+ */
+int main(void)
+{
+	printf("size 0f a char: %d byte(s)\n", sizeof(char));
+	 printf("size 0f a int: %d byte(s)\n", sizeof(int));
+	  printf("size 0f a long int: %d byte(s)\n", sizeof(long int));
+	   printf("size 0f a long long int: %d byte(s)\n", sizeof(long long int));
+	    printf("size 0f a float: %d byte(s)\n", sizeof(float));
+	    return(0);
+}
+
